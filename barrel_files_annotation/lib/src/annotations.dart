a90981c1963bf783d874337ca0e42a857c144b05
Anna Leushchenko <foxanna@users.noreply.github.com>
2023-10-15 19:21:57 +0300

    Document public API

--- barrel_files_annotation/lib/src/annotations.dart
+++ barrel_files_annotation/lib/src/annotations.dart
@@ -1,5 +1,11 @@
+/// Annotate top-level dart code element
+/// with `@includeInBarrelFile` or `@IncludeInBarrelFile()`
+/// for it to appear in the generated barrel file
 const includeInBarrelFile = IncludeInBarrelFile();
 
+/// Annotate top-level dart code element
+/// with `@includeInBarrelFile` or `@IncludeInBarrelFile()`
+/// for it to appear in the generated barrel file
 class IncludeInBarrelFile {
   const IncludeInBarrelFile();
 }

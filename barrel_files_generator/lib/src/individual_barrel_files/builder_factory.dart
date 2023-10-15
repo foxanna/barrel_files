a5ffb7ccca5b3ea79aa26974095b13599610fb69
Anna Leushchenko <foxanna@users.noreply.github.com>
2023-10-15 19:46:36 +0300

    Update documentation

--- barrel_files_generator/lib/src/individual_barrel_files/builder_factory.dart
+++ barrel_files_generator/lib/src/individual_barrel_files/builder_factory.dart
@@ -2,6 +2,8 @@ import 'package:barrel_files/src/individual_barrel_files/generator.dart';
 import 'package:build/build.dart';
 import 'package:source_gen/source_gen.dart';
 
+/// A builder to generate individual barrel files to be later combined
+/// into a single package barrel file by [barrelFilesBuilder]
 Builder individualBarrelFilesBuilder(BuilderOptions options) => LibraryBuilder(
       const IndividualBarrelFilesGenerator(),
       generatedExtension: '.barrel',
